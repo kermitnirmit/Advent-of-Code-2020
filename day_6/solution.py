705a32b975cc939fc97148bcfae56dc820608986
kermitnirmit <kermitnirmit@gmail.com>
2020-12-14 21:26:28 -0600

    day6

---
+++ day_6/solution.py
@@ -0,0 +1,13 @@
+groups = open("input.txt").read().rstrip().split("\n\n")
+
+# q1
+# print(sum(len(set(i.replace("\n", ""))) for i in groups))
+
+# q2
+# print(groups)
+qwer = []
+for group in groups:
+    e = group.split("\n")
+    q = [set(q) for q in e]
+    qwer.append((len(q[0].intersection(*q))))
+print(sum(qwer))
