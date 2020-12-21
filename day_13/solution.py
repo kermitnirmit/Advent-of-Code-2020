92d141326c6c58dc36752117d71bbbc41635ed85
kermitnirmit <kermitnirmit@gmail.com>
2020-12-21 00:32:00 -0600

    adds part 1 to day_13

--- day_13/solution.py
+++ day_13/solution.py
@@ -7,11 +7,19 @@ buses = []
 for i, x in enumerate(f[1].split(",")):
     if x.isdigit():
         buses.append((i, int(x)))
+a = -1000000000
+closestBusID = -1
+
+for bus in buses:
+    if est % bus[1] - bus[1]  > a:
+        a = est % bus[1] - bus[1]
+        closestBusID = bus[1]
+print(abs(a) * closestBusID)
+
 
 lcm = buses[0][1]
 for _, i in buses[1:]:
     lcm = lcm * i // gcd(lcm, i)
-
 
 smallLCM = buses[0][1]
 index = 1
